7b93187f49975255fef6a020fd55d535ec34c75d
Gaia <Gaia@erp.com>
2019-01-14 17:54:14 +0100

    add sobel filtering

--- watershed.py
+++ watershed.py
@@ -3,7 +3,13 @@ import queue
 from PIL import Image
 import sys
 import matplotlib.pyplot as plt
+from random import randint
+from math import sqrt
+
 import random
+import scipy
+import scipy.misc
+import scipy.ndimage
 
 
 INIT=-1
@@ -11,29 +17,66 @@ MASK=-2
 WSHED=0
 fictious_point = [-5,-5]
 def load_image( infilename ) :
-    img = Image.open( infilename ).convert('L')
-    img.load()
-    data = np.asarray( img, dtype=np.uint8)
-    return data
+    return scipy.misc.imread(infilename, mode="L")
 
 def save_image( npdata, outfilename ) :
-    img = Image.fromarray(npdata, 'L')
+    img = Image.fromarray(npdata)
     img.save( outfilename )
 
 def get_index(x,y, width):
     return x * width + y
 
+def conv(image, x, y, filtre):
+    sum = 0
+    center = len(filtre) // 2
+    for i in range(-center,
+                   center + 1):
+        for j in range(-center, center + 1):
+            if filtre[i + center][
+                j + center] != 0:
+                if 0 <= x + i < image.width and 0 <= y + j < image.height:
+                    xl = x + i
+                    yl = y + j
+                else:
+                    xl = x - i
+                    yl = y - j
+
+                if not 0 <= xl < image.width or not 0 <= yl < image.height:
+                    xl = x
+                    yl = y
+                sum += (image.getpixel((xl, yl)) * filtre[i + center][j + center])
+    return sum
+
+
+def sobel(file):
+    image = Image.open(file).convert('L')
+    res = image.copy()
+    x_mask = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+    y_mask = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+    for x in range(image.width):
+        for y in range(image.height):
+            dx = conv(image, x, y, x_mask)
+            dy = conv(image, x, y, y_mask)
+
+            val = int(round(sqrt(dx ** 2 + dy ** 2) * 255 / (
+                        sqrt(2) * 1020)))
+            res.putpixel((x, y), (val))
+    return np.array(res)
+
+
+
 def watershed(filename, output):
     current_label = 0
     current_dist = 0
     q = queue.Queue()
-    imi = load_image(filename)
+
+    imi = sobel(filename)
     imo = np.full(imi.shape, INIT)
     imd = np.full(imi.shape, 0)
 
-
     hmin = np.amin(imi)
     hmax = np.amax(imi)
+
 
     index = [ [] for elem in range(hmax-hmin+1)]
 
@@ -92,12 +135,24 @@ def watershed(filename, output):
                                 q.put([nx,ny])
                                 imo[nx,ny] = current_label
 
+    lpe = Image.new('RGB', (imo.shape[1], imo.shape[0]))
+    colors = [0]*(current_label + 1)
+    for c in range(len(colors)):
+        colors[c] = (randint(100, 200), randint(100, 200), randint(100, 200))
 
-    I8 = (((imo - imo.min()) / (imo.max() - imo.min())) * 255).astype(np.uint8)
-    save_image(I8, output)
+    for i in range(imo.shape[1]):
+        for j in range(imo.shape[0]):
 
-    plt.imshow(imo)
+            if imo[j][i] == WSHED:
+                lpe.putpixel((i, j), (0,0,0))
+            else:
+                lpe.putpixel((i, j), colors[imo[j][i]])
+
+    plt.imshow(lpe)
     plt.show()
+
+    save_image(imi, "gradient.jpg")
+    lpe.save(output)
 
     return
 
